392f09a390be491a87aab26f6a9cf5b9ec43ea38
Alesya Polovets <alesya.polovets@gmail.com>
2018-09-24 22:29:56 +0300

    zeros/feature: implemented getZerosCount function, which calculate how many zeros in the end of result number, which is factorial of number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
   // your implementation
-}
+    let zerosCount = 0;
+    for (let i = 1; Math.pow(5, i) <= number; i++) {
+        zerosCount += Math.floor(number / (Math.pow(5, i)));
+    }
+    return zerosCount;
+};
